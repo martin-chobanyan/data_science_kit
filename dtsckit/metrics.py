27be789e0dd5fd91685add7d0d7813a897ade9dc
Martin Chobanyan <chobanyan.martin@gmail.com>
2018-12-18 00:42:48 -0500

    integrated early stopping and new tensor metrics into the old files

--- dtsckit/metrics.py
+++ dtsckit/metrics.py
@@ -26,6 +26,12 @@ class AverageKeeper(object):
         self.sum = 0
         self.n = 0
 
+
+#####################################
+######PYTORCH HELPER FUNCTIONS#######
+#####################################
+
+
 # TODO move this to model.py
 def softmax_pred(linear_out):
     """Apply softmax and get the predictions
@@ -43,3 +49,57 @@ def softmax_pred(linear_out):
     pred = torch.argmax(softmax_out, dim=1)
     pred = pred.squeeze()
     return pred.numpy()
+
+
+def binary_breakdown(x):
+    """Number of positive and negative examples in a binary tensor
+    
+    Parameters
+    ----------
+    x: torch.tensor
+        A tensor consisting of only ones and zeros (or True/False)
+        
+    Returns
+    -------
+    A tuple(int, int) for the number of postive and negative examples, respectively
+    """
+    n_elements = np.prod(x.size()).item()
+    n_pos = x.sum().item()
+    n_neg = n_elements - n_pos
+    return n_pos, n_neg
+
+
+def tensor_cnf_mtrx(gt, pred):
+    """Tensor confusion matrix
+    
+    Create a confusion matrix from a tensor of binary variables (0s and 1s).
+    The matrix will be ordered as such
+    
+    ----------------------------------------
+    |  True Positive   |   False Negative  |
+    ----------------------------------------
+    |  False Positive  |   True Negative   |
+    ----------------------------------------
+    
+    Parameters
+    ----------
+    gt: torch.tensor
+        The ground truth tensor
+    pred: torch.tensor
+        The predicted tensor
+        
+    Returns   
+    -------
+    The confusion matrix as a 2x2 numpy array
+    """
+    
+    # get the confusion results
+    gt, pred = gt.float(), pred.float()
+    confusion_results = pred / gt
+    
+    tp = torch.sum(confusion_results == 1).item()
+    fn = torch.sum(confusion_results == 0).item()
+    fp = torch.sum(confusion_results == float('inf')).item()
+    tn = torch.sum(torch.isnan(confusion_results)).item()
+    
+    return np.array([[tp, fn], [fp, tn]])
--- dtsckit/model.py
+++ dtsckit/model.py
@@ -84,3 +84,40 @@ def validate_epoch(epoch, model, dataloader, criterion, device, print_rate=50):
     return loss_avg.calculate()
 
 
+# TODO: patch this up so that it works...
+def early_stop(train_loader, eval_loader, model, optimizer, criterion,
+               maxepochs, device, n=1, patience=3):
+
+    epoch_i = 0
+    patience_i = 0
+    best_validation_loss = float('inf')
+    stop = 0
+
+    # while the validation loss has not consistently increased
+    while patience_i < patience:
+
+        # train the model for n steps
+        for i in range(n):
+
+            if epoch_i == maxepochs:
+                return stop
+
+            train(epoch_i, train_loader, model, optimizer, criterion, device)
+            epoch_i += 1
+
+        # get the validation loss
+        validation_loss = validate(epoch_i, eval_loader, model, criterion, device)
+
+        if validation_loss < best_validation_loss:
+
+            patience_i = 0
+            best_validation_loss = validation_loss
+
+            # save the model and update the stopping epoch
+            checkpoint(args.outputfolder, epoch_i, model, best_validation_loss)
+            stop = epoch_i
+
+        else:
+            patience_i += 1
+
+    return stop
